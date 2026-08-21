364bc1e41a090455f98ab6b854e6221de07d1298
Pruthvi <pruthvims712@gmail.com>
2026-08-21 08:42:34 +0530

    Optimize website images

--- src/lib/site-images.ts
+++ src/lib/site-images.ts
@@ -1,15 +1,23 @@
-import companyOwner from "../../Images/Company owner.png";
-import entranceOfficeGuards from "../../Images/Entrance Office Guards.png";
-import gateSecurity from "../../Images/Gate Security.png";
-import soloGuard from "../../Images/Solo Guard.png";
-import techParkGuards from "../../Images/Tech park Guards.png";
-import techPark from "../../Images/Tech park.png";
-import corporateEvent from "../../Images/Corporate Event.png";
-import receptionGuard from "../../Images/Reception Guard.png";
-import officeFront from "../../Images/Office front.png";
-import entranceDark from "../../Images/Entrance dark.png";
+import companyOwner from "../../Images/Company owner.webp";
+import entranceOfficeGuards from "../../Images/Entrance Office Guards.webp";
+import gateSecurity from "../../Images/Gate Security.webp";
+import soloGuard from "../../Images/Solo Guard.webp";
+import techParkGuards from "../../Images/Tech park Guards.webp";
+import techPark from "../../Images/Tech park.webp";
+import corporateEvent from "../../Images/Corporate Event.webp";
+import receptionGuard from "../../Images/Reception Guard.webp";
+import officeFront from "../../Images/Office front.webp";
+import entranceDark from "../../Images/Entrance dark.webp";
 
-export { companyOwner, entranceOfficeGuards, gateSecurity, soloGuard, techParkGuards, techPark ,corporateEvent,
-receptionGuard,
-officeFront,
-entranceDark,};
+export {
+  companyOwner,
+  entranceOfficeGuards,
+  gateSecurity,
+  soloGuard,
+  techParkGuards,
+  techPark,
+  corporateEvent,
+  receptionGuard,
+  officeFront,
+  entranceDark,
+};
